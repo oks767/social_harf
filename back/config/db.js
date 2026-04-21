263a3fb25fc7dc2117955a639807b9ce4c8a99bc
oks767 <aleksandrovao767oks@yandex.ru>
2026-04-21 08:02:55 +0300

    создание подключения к серверу

--- back/config/db.js
+++ back/config/db.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose'
 import { User } from '../models/user.js';
-const MONGO_URL = 'mongodb://localhost:27017/social';
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/social';
 
-async function main() {
+export async function main() {
   try {
     await mongoose.connect(MONGO_URL);
-    console.log('✅ Подключено к MongoDB');
+    console.log('Подключено к MongoDB');
     const existingUser = await User.findOne({ email: 'test@test.com' });
     console.log('Найден пользователь:', existingUser);
     if (!existingUser) {
       const newUser = new User({ email: 'test@test.com', password: '123' });
       await newUser.save();
-      console.log('✅ Пользователь создан');
+      console.log('Пользователь создан');
     }
   } catch (error) {
     console.error('Ошибка:', error);
--- back/config/server.js
+++ back/config/server.js
@@ -1,3 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { main } from './db.js'
 dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+await main()
+
+app.get('/', (req, res) => {
+    res.send('hello, world')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+main()
